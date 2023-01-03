da84c41fc3c003d59cd753f14f1e22cb99d1b976
Amanda Elvarsdóttir <amandahelga@Amandas-MacBook-Pro.local>
2023-01-03 17:02:29 -0600

    fixing result table issue

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const Results = (searchResults) => {
+const Results = (props) => {
+  const {searchResults} = props;
+  console.log('searchResults ', searchResults)
     
     return (
         <div className="search-result">
@@ -11,14 +13,14 @@ const Results = (searchResults) => {
                     <th>Shipping Number</th>
                 </tr>
                 </thead>
-                {searchResults.map((info) => {
+                {searchResults.map((info) =>
                 <tbody>
                     <tr>
                         <td>{info.label_id}</td>
                         <td>{info.shipping_tracking_code}</td>
                     </tr>
                 </tbody>
-})}
+)}
                        
             </table>
         </div>
